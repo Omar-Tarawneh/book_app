bd9153e6344202c656441a34f6f31c5771cef910
Omar Hakem <youtarawneh997@gmail.com>
2021-03-01 18:53:44 +0200

    fixing the authonication

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const pg = require('pg');
 const app = express();
 app.use(cors());
 require('dotenv').config();
-const client = new pg.Client(process.env.DATABASE_URL);
-// const client = new pg.Client({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } });
+// const client = new pg.Client(process.env.DATABASE_URL);
+const client = new pg.Client({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } });
 // PORT
 const PORT = process.env.PORT;
 
